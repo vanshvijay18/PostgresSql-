3b1fe0ae04487946cdf50fa14cf8e4cfb62d44c6
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-27 21:31:37 +0530

    Create InsertValue.sql

---
+++ InsertValue.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (name, department, salary, joining_date)
+VALUES
+('Aman', 'IT', 50000, '2024-01-10'),
+('Rohit', 'HR', 40000, '2023-06-15'),
+('Neha', 'IT', 60000, '2022-09-20');
