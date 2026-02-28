ccac270cb077f58624e3080a609b8c25a92e325e
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-28 12:06:45 +0530

    Create SelectQueries.sql

---
+++ SelectQueries.sql
@@ -0,0 +1,11 @@
+-- Select All
+SELECT * FROM employee;
+
+--Select Specific Columns
+SELECT name, salary FROM employee;
+
+--Where Clause
+SELECT * FROM employee WHERE department = 'IT';
+
+--AND / OR
+SELECT * FROM employee
