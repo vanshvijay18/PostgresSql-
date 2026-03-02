2dc9785de02c9b650863c859e76984061fe97057
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-03-02 20:28:19 +0530

    Create InsertValue.sql

---
+++ Joins/InsertValue.sql
@@ -0,0 +1,11 @@
+INSERT INTO department VALUES
+(1, 'IT'),
+(2, 'HR'),
+(3, 'Finance'),
+(4, 'Admin');
+
+INSERT INTO employee VALUES
+(101, 'Aman', 50000, 1),
+(102, 'Rohit', 40000, 2),
+(103, 'Neha', 60000, 1),
+(104, 'Pooja', 45000, NULL);
