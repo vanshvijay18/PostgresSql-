ce78bad8cdcc12ab8e049a32724b3460452adc91
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-03-06 23:37:44 +0530

    Create Sample.sql

---
+++ WindowFunctions/Sample.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employee (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    department VARCHAR(30),
+    salary INT
+);
+
+INSERT INTO employee VALUES
+(1,'Aman','IT',50000),
+(2,'Rohit','IT',60000),
+(3,'Neha','HR',45000),
+(4,'Pooja','HR',40000),
+(5,'Karan','IT',70000);
