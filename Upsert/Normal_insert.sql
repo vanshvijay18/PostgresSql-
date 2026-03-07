aacf5bacbedacfa0370bd7a959dc9bf6d5efdc4c
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-03-07 14:40:23 +0530

    Create Normal_insert.sql

---
+++ Upsert/Normal_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers VALUES
+(1, 'Aman', 'aman@email.com');
+-- If we run again:
+INSERT INTO customers VALUES
+(1, 'Aman', 'aman@email.com');
+
+-- ❌ Error:
+-- duplicate key value violates unique constraint
