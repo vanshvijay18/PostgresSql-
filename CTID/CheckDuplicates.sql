40d15c41d9743c47defda4554b983311ca0ebef9
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-03-12 21:30:41 +0530

    Create CheckDuplicates.sql

---
+++ CTID/CheckDuplicates.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM employees;
+
+
+
+| id | name  | department |
+| -- | ----- | ---------- |
+| 1  | Aman  | IT         |
+| 1  | Aman  | IT         |
+| 2  | Rohit | HR         |
+| 2  | Rohit | HR         |
+| 3  | Neha  | Finance    |
